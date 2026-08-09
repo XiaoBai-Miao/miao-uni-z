1fccecc8e0913a0471518b6f0c776affa9a93e44
Xiao-Bai-Miao <1842072675@qq.com>
2026-08-09 16:11:43 +0800

    v1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.miao"
         minSdk = 28
         targetSdk = 37
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
